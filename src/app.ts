afe3248dbce34352b2c45323a5e3605d413b1bcf
guirdy <guilherme.gl1997@gmail.com>
2023-07-17 12:23:45 -0300

    feat: prisma orm integration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Guilherme Leandro',
+    email: 'guilherme@leandro.com.br',
+  },
+})
+
+// ORM - Object Relational Mapper
